898a55856f1c6248338ea7ac83f461b53f2ae237
Patrick Paulino <pmpaulino@gmail.com>
2024-09-23 17:07:44 -0700

    Update dagger configuration

--- dagger/src/main/__init__.py
+++ dagger/src/main/__init__.py
@@ -1,6 +1,30 @@
+"""A generated module for HelloDaggerFastapiRye functions
+
+This module has been generated via dagger init and serves as a reference to
+basic module structure as you get started with Dagger.
+
+Two functions have been pre-created. You can modify, delete, or add to them,
+as needed. They demonstrate usage of arguments and return types using simple
+echo and grep commands. The functions can be called from the dagger CLI or
+from one of the SDKs.
+
+The first line in this comment block is a short description line and the
+rest is a long description with more detail on the module's purpose or usage,
+if appropriate. All modules should have a short description.
+"""
+
+# NOTE: it's recommended to move your code into other files in this package
+# and keep __init__.py for imports only, according to Python's convention.
+# The only requirement is that Dagger needs to be able to import a package
+# called "main" (i.e., src/main/).
+#
+# For example, to import from src/main/main.py:
+# >>> from .main import HelloDaggerFastapiRye as HelloDaggerFastapiRye
+
+from typing import Annotated
+
 import dagger
-import random
-from dagger import dag, function, object_type
+from dagger import Doc, dag, function, object_type
 
 
 @object_type
@@ -53,5 +77,54 @@ class HelloDaggerFastapiRye:
         # Call Dagger Function to build the application image
         # Publish the image to ttl.sh
         return await self.build(source).publish(
-            f"ttl.sh/myapp-{random.randrange(10 ** 8)}"
+            f"ttl.sh/myapp-{random.randrange(10 ** 8)}:latest"
         )
+
+    @function
+    def kustomize(
+        self, directory_arg: dagger.Directory, env: str | None = "nonprod"
+    ) -> dagger.File:
+        """
+        dagger -vvv call \
+        kustomize --directory_arg=k8s \
+        export --path k8s_test/nonoprod.yaml
+        """
+        return (
+            dag.container()
+            .from_("registry.k8s.io/kustomize/kustomize:v5.0.0")
+            .with_mounted_directory("/mnt", directory_arg)
+            .with_workdir("/mnt")
+            .with_exec(
+                [
+                    "kustomize",
+                    "build",
+                    f"overlays/{env}",
+                    "-o",
+                    f"manifests/{env}/manifests.yaml",
+                ]
+            )
+            .file(f"/mnt/manifests/{env}/manifests.yaml")
+        )
+
+    # TODO: Take in image name and run kustomize
+    # @function
+    # def kustomize(self, image_full_name: str, source: dagger.Directory) -> str:
+    #     """Update the kustomization.yaml file with the new image URL"""
+    #     try:
+    #         # Extract the image name and tag
+    #         image_name, image_tag = image_full_name.split('@')[0], image_full_name.split(':')[-1]
+
+    #         # Load the kustomization.yaml file
+    #         with open("k8s/base/kustomization.yaml", "r") as file:
+    #             kustomization = yaml.safe_load(file)
+
+    #         # Update the image details
+    #         kustomization['images'][0]['newName'] = image_name
+    #         kustomization['images'][0]['newTag'] = image_tag
+
+    #         # Write the updated kustomization.yaml file
+    #         with open("k8s/base/kustomization.yaml", "w") as file:
+    #             yaml.safe_dump(kustomization, file)
+
+    #     except Exception as e:
+    #         print(f"Failed to update kustomization.yaml: {e}")
